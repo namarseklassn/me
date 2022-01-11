29486f7e7a46c62deea1736ec108a5ce28c9b3a3
namarseklassn <92542583+namarseklassn@users.noreply.github.com>
2022-01-11 21:30:25 +0300

    Add files via upload

---
+++ js/JS2.js
@@ -0,0 +1,52 @@
+/*let x = 'you are welcome';
+for(let m = 1; m<=10; m++) {console.log (x)}*/
+
+/*let x = 1001;
+for( let i=1001; i <= 1025; i = i+3) {console.log(i);}*/
+
+/*let x = '*';
+for( let i=1; i<=6; i++) {console.log(x);
+x = x + "*"}*/
+
+/*let x = '****';
+let y = '*******';
+for(let i=1; i<=30; i++) {
+    if (i%2==0) {
+    console.log(x)}
+     else{console.log(y)
+    }
+}*/
+
+/*let x=1.2;
+for(let i=1.2; i<=2.81; i=i+0.2){
+    console.log(i);}*/
+
+    /*for(let i = 1; i<=10; i++){
+        for(let n = 1;n<=10; n++){
+            let m=i*n;
+            console.log(`${i}*${n}=${m}`);
+        }
+        console.log('------------');
+    }*/
+
+    /*let n=10;
+    for(let x=1; x<=n; x++) {
+        let s=String(x);
+        for(let i=1; i<=x*n; i++){
+            s=s+String(x);
+        }
+        console.log(s);
+    }*/
+
+    /*let x=0;
+    for(let n=1; n<=800; n++ ){
+        x=x +1/n;}
+        console.log(x);*/
+
+
+        let n=0;
+        for(let x=1000; x<=9999; x++){
+            if(x%3==0) {n=n+x;
+            }
+        }
+            console.log(n);
