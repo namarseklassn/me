7e4684fe1fef0247ad9a461c9f8ef82c191cd11e
namarseklassn <92542583+namarseklassn@users.noreply.github.com>
2022-01-11 18:32:26 +0300

    Add files via upload

---
+++ js/newJS.js
@@ -0,0 +1,25 @@
+/*console.log('Silence is golden');
+let a = 1;
+let b = 2;
+let c = 4;
+console.log(a + b - c);
+console.log(a / b + a / c);
+a = 2;
+b = 3;
+let d = a ** a;
+console.log((a + 4 * b) * (a - 3 * b) + d);
+a = +prompt("первое число");
+b = +prompt("второе число");
+alert(a+b);
+a = +prompt("квадрат");
+alert(a**2);
+b = +prompt("куб");
+alert(b**3);
+a = 3;
+c = prompt("введи число");
+if (c > a) {c = a + 10} else {c = a - 10};
+alert(c);*/
+let c = prompt("введи число");
+ if(c < 7) {console.log(c = "yes")}
+ if(c > 10) {console.log(c = "No")}
+ if(c == 9) {console.log(c = "Eror")}
