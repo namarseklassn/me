eabcf740cfadcedaa20fc94b896c0c417a28bd6e
namarseklassn <92542583+namarseklassn@users.noreply.github.com>
2022-01-13 21:19:06 +0300

    Add files via upload

---
+++ js/JS3.js
@@ -0,0 +1,24 @@
+/*let s ='Корпорация Майкрософт. Все права защищены'
+let a = s.slice(0,1);
+console.log(a);
+a=s.slice(-1);
+console.log(a);
+if (s.length % 2 != 0) {
+let n=(s.length -1)/2;
+console.log(s[n]);
+}*/
+
+/*let s = 'Корпорация Майкрософт. Все права защищены';
+s = s.replace('Все', 'Не все');
+console.log(s);*/
+
+/*let s = "acdew"
+if ( s.startsWith('abc')) {s=s.replace('abc', 'www')} 
+else {s=s + 'zzz'};
+console.log(s);*/
+
+/*let s = 'qwertyuasdfghzxcvb';
+if(s.indexOf('x') > s.indexOf('y')) {s='y'}
+else if(s.indexOf('x') < s.indexOf('y')) {s='x'}
+else {s='net takih'};
+console.log(s);*/
