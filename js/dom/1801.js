6d1cf83030adf07564102c74d1b25a918ddcee59
namarseklassn <92542583+namarseklassn@users.noreply.github.com>
2022-02-01 04:27:50 +0300

    Add files via upload

---
+++ js/dom/1801.js
@@ -0,0 +1,12 @@
+// 1zadacha 
+// let s = 'asd!asdf!adf!asdf!!!!'
+// // s=s.replace('!','')
+// // console.log(s);
+// for(let a='!';a<0;a++) {
+//     a=s.replace('!','')
+//     }
+//     console.log(s)
+// 2zadacha
+let s = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+let a =words.length(s)
+console.log(a);
---
+++ js/dom/dz0202.js
@@ -0,0 +1,31 @@
+function Train (city, nomber,time) {
+    this.cityq = city,
+    this.nomberq = nomber,
+    this.timeq = time,
+    this.Poryadok = function() {}
+} 
+const tr1 = new Train ('Minsk', 4212, [1200, 2100,1700,2300,2350]);
+const tr2 = new Train ('Pinsk', 121113, [1600,1700,1555, 1750,2416]);
+const tr3 = new Train ('Moskow', 7214, [0900,2211,2418,1233,1122]);
+const tr4 = new Train ('Ufa', 215, [2000,1234,2145,2356,0715]);
+const train = [tr1,tr2,tr3,tr4];
+const poryad = train.sort ((t1, t2) => {
+    return t1.nomberq - t2.nomberq;
+});
+console.log(poryad);
+// ---------------------
+let vvod = prompt ('номер поезда')
+if (vvod = '4212') {console.log (vvod = tr1)} 
+if (vvod = '121113') {console.log (vvod = tr2)};
+if (vvod = '7214') {console.log (vvod = tr3)};
+if (vvod = '215') {console.log (vvod = tr4)};
+if (vvod != tr1 &&
+    vvod != tr2 &&
+    vvod != tr3 &&
+    vvod != tr4 ) {console.log (vvod = 'Нет такого номера')};
+// ---------------------
+const trainSort = [tr1[2],tr2[2],tr3[2],tr4[2]];
+const porGor = trainSort.sort((p1,p2) => {
+    return p1.timeq-p2.timeq;
+});
+console.log(porGor);
